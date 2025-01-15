b822dc321ba41b68589934adefbef33b93e91098
nahla-anwar <nahlaanwar2211@gmail.com>
2025-01-15 17:21:16 +0200

    second commit

--- js/index.js
+++ js/index.js
@@ -1,7 +1,21 @@
+// * HTML elements
+const navbar = document.getElementById("navbar");
 const navBtn = document.getElementById("navBtn");
 const trialBtn = document.getElementById("trialBtn");
 const lightBox = document.getElementById("lightBox");
 const lightBoxBtn = document.getElementById("lightBoxBtn");
+const chatBoxIcon = document.getElementById("chatBoxIcon");
+const closeChatBtn = document.getElementById("closeChatBtn");
+const chatBox = document.getElementById("chatBox");
+
+// * functions
+function changeNavbar() {
+  if (window.scrollY > 20) {
+    navbar.classList.add("navbarChange");
+  } else {
+    navbar.classList.remove("navbarChange");
+  }
+}
 
 function showLightBox() {
   lightBox.classList.replace("d-none", "d-flex");
@@ -10,6 +24,17 @@ function showLightBox() {
 function hideLightBox() {
   lightBox.classList.replace("d-flex", "d-none");
 }
+
+function showHideChatBox() {
+  chatBox.classList.toggle("show");
+}
+
+function hideChatBox() {
+  chatBox.classList.remove("show");
+}
+
+// * events
+window.addEventListener("scroll", changeNavbar);
 
 navBtn.addEventListener("click", showLightBox);
 trialBtn.addEventListener("click", showLightBox);
@@ -21,3 +46,6 @@ lightBox.addEventListener("click", function (e) {
     hideLightBox();
   }
 });
+
+chatBoxIcon.addEventListener("click", showHideChatBox);
+closeChatBtn.addEventListener("click", hideChatBox);
